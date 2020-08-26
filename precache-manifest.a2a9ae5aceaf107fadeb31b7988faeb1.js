c06826003467dc8328c3a5ca2a3293843a56de31
asmithwash101 <abswashington98@gmail.com>
2020-08-26 16:54:55 -0500

    Updates

---
+++ precache-manifest.a2a9ae5aceaf107fadeb31b7988faeb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a14999eb0ec893e58220a1d30b7135",
+    "url": "/shonachat/index.html"
+  },
+  {
+    "revision": "1276302cca8e5ffb3632",
+    "url": "/shonachat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7011607f8bb1330b36a0",
+    "url": "/shonachat/static/js/2.03768984.chunk.js"
+  },
+  {
+    "revision": "ad19ffc35cbdd94e7387304fcacd6f6e",
+    "url": "/shonachat/static/js/2.03768984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1276302cca8e5ffb3632",
+    "url": "/shonachat/static/js/main.0381a1a7.chunk.js"
+  },
+  {
+    "revision": "1dea673c5b98f22dd264",
+    "url": "/shonachat/static/js/runtime-main.89a3b9dc.js"
+  }
+]);
